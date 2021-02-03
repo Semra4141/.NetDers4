93f1540fe1807af6c1c191c6731feaa1461a2a30
Semra Güneş <60344956+Semra4141@users.noreply.github.com>
2021-02-03 23:12:17 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+
+            product1.ID=0;
+            product1.CategoryId=1;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 5;
+
+            ProductManager productManeger = new ProductManager();
+
+           
+
+        }
+    }
+}
